719a3cc10e353af13293029c0c71a2c2811b09ee
Matthew Berthoud <mwberthoud@wm.edu>
2024-11-12 21:07:57 -0500

    done with homework

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+document.getElementById("submitButton").addEventListener("click", addTask);
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput').value;
+    const newTask = document.createElement('li');
+    const deleteButton = document.createElement("button");
+    const taskList = document.getElementById('taskList');
+
+    newTask.innerText = taskInput;
+    deleteButton.className="button";
+    deleteButton.innerText="x";
+
+    newTask.appendChild(deleteButton);
+    taskList.appendChild(newTask);
+
+    deleteButton.addEventListener("click", () => taskList.removeChild(newTask));
+    document.getElementById('taskInput').value='';
+}
 
